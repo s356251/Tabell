47804605a7091cd1e5e11f57a64a504c4ff715fd
erik <s356251@oslomet.no>
2022-09-01 11:28:54 +0200

    Opp til 1.2.2, oppgave 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args){
 
+        int[] a = Tabell.randPerm(20);              // en tilfeldig tabell
+        for (int k : a) System.out.print(k + " ");  // skriver ut a
 
+        int m = Tabell.maks(a);   // finner posisjonen til største verdi
+
+        System.out.println("\nStørste verdi ligger på plass " + m);
     }
 
 }
--- src/Tabell.java
+++ src/Tabell.java
@@ -1,50 +1,9 @@
+import java.util.Arrays;
+import java.util.Random;
 public class Tabell {
+    private Tabell() {}   // privat standardkonstruktør - hindrer instansiering
 
-    public static int maks(int[] a, int fra, int til)  // a er en heltallstabell
-    {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
-
-        int funnetmaks = a[fra];
-        int indeks = fra;
-
-        for( int i = fra; i < til; i++){
-            if (a[i] < funnetmaks){
-                funnetmaks = a[i];
-                indeks = i;
-            }
-        }
-        return indeks;
-    } // maks
-    public static int maks(int[] a)  // bruker hele tabellen
-    {
-        return maks(a,0,a.length);     // kaller metoden over
-    }
-
-    public static int min(int[] a, int fra, int til)  // a er en heltallstabell
-    {
-        if (fra < 0 || til > a.length || fra >= til)
-        {
-            throw new IllegalArgumentException("Illegalt intervall!");
-        }
-
-        int funnetmin = a[fra];
-        int indeks = fra;
-
-        for( int i = fra; i < til; i++){
-            if (a[i] > funnetmin){
-                funnetmin = a[i];
-                indeks = i;
-            }
-        }
-        return indeks;
-    } // min
-    public static int min(int[] a)  // bruker hele tabellen
-    {
-        return min(a,0,a.length);     // kaller metoden over
-    }
+    // Metoden bytt(int[] a, int i, int j)       Programkode 1.1.8 d)
     public static int[] bytt(int[] a, int x, int y){ //Bytter om to gitte indekser i et heltallsarray
         int temp = a[x];
         a[x] = a[y];
@@ -52,7 +11,83 @@ public class Tabell {
 
         return a;
     }
+    // Metoden randPerm(int n)                   Programkode 1.1.8 e)
+    public static int[] randPerm(int n)  // en effektiv versjon
+    {
+        Random r = new Random();         // en randomgenerator
+        int[] a = new int[n];            // en tabell med plass til n tall
 
+        Arrays.setAll(a, i -> i + 1);    // legger inn tallene 1, 2, . , n
+
+        for (int k = n - 1; k > 0; k--)  // løkke som går n - 1 ganger
+        {
+            int i = r.nextInt(k+1);        // en tilfeldig tall fra 0 til k
+            bytt(a,k,i);                   // bytter om
+        }
+
+        return a;                        // permutasjonen returneres
+    }
+    // Metoden randPerm(int[] a)                 Programkode 1.1.8 f)
+    public static void randPerm(int[] a)  // stokker om a
+    {
+        Random r = new Random();     // en randomgenerator
+
+        for (int k = a.length - 1; k > 0; k--)
+        {
+            int i = r.nextInt(k + 1);  // tilfeldig tall fra [0,k]
+            bytt(a,k,i);
+        }
+    }
+    // Metoden maks(int[] a, int fra, int til)   Programkode 1.2.1 b)
+    public static int maks(int[] a, int fra, int til)
+    {
+        if (fra < 0 || til > a.length || fra >= til)
+        {
+            throw new IllegalArgumentException("Illegalt intervall!");
+        }
+
+        int m = fra;              // indeks til største verdi i a[fra:til>
+        int maksverdi = a[fra];   // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+        {
+            if (a[i] > maksverdi)
+            {
+                m = i;                // indeks til største verdi oppdateres
+                maksverdi = a[m];     // største verdi oppdateres
+            }
+        }
+
+        return m;  // posisjonen til største verdi i a[fra:til>
+    }
+    // Metoden maks(int[] a)                     Programkode 1.2.1 c)
+    public static int maks(int[] a)  // bruker hele tabellen
+    {
+        return maks(a,0,a.length);     // kaller metoden over
+    }
+
+    // min-metodene - se Oppgave 1 i Avsnitt 1.2.1
+    public static int min(int[] a, int fra, int til) {
+        if (fra < 0 || til > a.length || fra >= til) {
+            throw new IllegalArgumentException("Illegalt intervall!");
+        }
+
+        int m = fra;              // indeks til største verdi i a[fra:til>
+        int minverdi = a[fra];   // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++) {
+            if (a[i] < minverdi) {
+                m = i;                // indeks til største verdi oppdateres
+                minverdi = a[m];     // største verdi oppdateres
+            }
+        }
+        return m;  // posisjonen til minste verdi i a[fra:til>
+    }
+
+    public static int min(int[] a)  // bruker hele tabellen
+    {
+        return min(a,0,a.length);     // kaller metoden over
+    }
     public static int[] minmaks(int[] a){
         int maks = maks(a, 0, a.length);
         int min = min(a, 0, a.length);
@@ -69,9 +104,9 @@ public class Tabell {
         return a;
     }
 
-    public static int[] lagArray(int lengde){
+    public static int[] lagArray(int lengde) {
         int[] a = new int[lengde];
-        for (int i = 0; i < lengde; i++){
+        for (int i = 0; i < lengde; i++) {
             a[i] = i;
         }
         return a;
