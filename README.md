cd038754436fa7f6a27b45338f9e2b56f20ea716
erik <s356251@oslomet.no>
2022-09-06 09:25:40 +0200

    Oppdatert Readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tabell
 Klasse Tabell til videre arbeid i semesteret
 
+DATS2300 - Algoritmer og Datastrukturer
+Ved OsloMet høst 2022
+
+Fra kompendie ved https://www.cs.hioa.no/~ulfu/appolonius/index.html
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,21 @@ public class Main {
         int m = Tabell.maks(a);   // finner posisjonen til største verdi
 
         System.out.println("\nStørste verdi ligger på plass " + m);
+
+        int[] b = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
+        int[] c = Tabell.nestMaks(a);  // metoden returnerer en tabell
+
+        int x = c[0], nm = c[1];       // m for maks, nm for nestmaks
+
+        Tabell.skrivln(a);        // se Oppgave 5 i Avsnitt 1.2.2
+        System.out.print("Størst(" + a[m] + ") har posisjon " + m);
+        System.out.println(", nest størst(" + a[nm] + ") har posisjon " + nm);
+
+        // Eksempel på en utskrift:
+
+        // 12 16 15 6 10 8 9 2 14 19 5 18 20 13 3 7 11 1 4 17
+        // Størst(20) har posisjon 12, nest størst(19) har posisjon 9
     }
+
 
 }
